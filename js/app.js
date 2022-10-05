52901d0c7a26dcaa6e9c65ea67efd72c88991bed
Domaine Scully <dalexscully@gmail.com>
2022-10-05 11:09:30 -0400

    Updated files

--- js/app.js
+++ js/app.js
@@ -1,8 +1,14 @@
 'use strict';
 
 // ******** GLOABAL VARIABLES **********
+<<<<<<< HEAD
 // let wineArray = [];
 // let indexArray = [];
+=======
+let wineArray = [];
+let matchwine = [];
+let clickimg = '';
+>>>>>>> 20e2523c0ae9663891d59b4e5db5319083fd7f7e
 
 
 
@@ -13,6 +19,7 @@
 // ******** JS VIEWS SOURCE AS PROPERTY *******
 // let imgOne = document.getElementById('imgone');
 // let imgTwo = document.getElementById('imgtwo');
+<<<<<<< HEAD
 // let imgThree = document.getElementById('imgthree');
 // let imgFour = document.getElementById('imgone');
 // let imgFive = document.getElementById('imgtwo');
@@ -103,3 +110,113 @@
 // renderImgs();
 
 // imgContainer.addEventListener('click', handleClick);
+=======
+
+//  ******** CONSTRUCTOR FUNCTION ********
+
+function Wine(name, category, fileExtension = 'png') {
+  this.name = name;
+  this.img = `./img/${name}.${fileExtension}`;
+  this.category = category;
+
+
+
+  wineArray.push(this);
+}
+
+
+
+
+
+//  Event Handler
+
+function handleClick(event) {
+  console.log(event.target);
+
+  let categoryname = event.target.alt;
+  clickimg = event.target.alt;
+  localStorage.setItem('clicked', clickimg);
+  for (let i = 0; i < wineArray.length; i++) {
+    for (let j = 0; j < wineArray[i].category.length; j++) {
+      wineArray[i].category[j] === categoryname;
+      matchwine.push(wineArray[i].img);
+    }
+  }
+
+  console.log(matchwine);
+  console.log(wineArray);
+  console.log(clickimg);
+}
+
+
+function retrieveWine() {
+  let wine = localStorage.getItem('mywine');
+  if (wine) {
+    let parseWine = JSON.parse(wine);
+    matchwine = parseWine;
+  } else {
+    new Wine('cab1', ['steak']);
+    new Wine('cab2', ['steak']);
+    new Wine('sweet1', ['fish']);
+    new Wine('sweet2', ['fish']);
+  }
+  console.log(wine);
+}
+
+
+function saveWine() {
+
+  let stringwine = JSON.stringify(wineArray);
+  localStorage.setItem('myWine', stringwine);
+}
+
+
+
+
+
+let imgTwo = document.getElementById('twoWine');
+
+function renderWine() {
+
+  let clicked = localStorage.getItem('clicked');
+  for (let i = 0; i < wineArray.length; i++) {
+    for (let j = 0; j < wineArray[i].category.length; j++) {
+      console.log(wineArray[i].category[j]);
+      if (wineArray[i].category[j] === clicked) {
+        console.log('we did it');
+
+        let imgOne = document.getElementById('img-container');
+        let imgOneelem = document.createElement('img');
+        imgOneelem.setAttribute('src', wineArray[i].img);
+        imgOne.appendChild(imgOneelem);
+        // imgOne.src = matchwine[i].img;
+        // imgTwo.src = matchwine[i].img;
+        // imgOne.alt = wineArray[i].name;
+        // imgTwo.alt = wineArray[i].name;
+      }
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+
+retrieveWine();
+
+
+renderWine();
+
+
+saveWine();
+
+
+
+
+imgContainer.addEventListener('click', handleClick);
+>>>>>>> 20e2523c0ae9663891d59b4e5db5319083fd7f7e
